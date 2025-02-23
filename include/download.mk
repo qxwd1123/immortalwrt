qxwd1123/openwrt

do not try source.openwrt.org for packaged source code

include/download.mk
@@ -195,23 +195,19 @@ define DownloadMethod/svn
 endef
 
 define DownloadMethod/git
-	$(call wrap_mirror,$(1),$(2), \
-		$(call DownloadMethod/rawgit) \
-	)
+	$(call DownloadMethod/rawgit)
 endef
 
 define DownloadMethod/github_archive
-	$(call wrap_mirror,$(1),$(2), \
-		$(SCRIPT_DIR)/dl_github_archive.py \
-			--dl-dir="$(DL_DIR)" \
-			--url="$(URL)" \
-			--version="$(SOURCE_VERSION)" \
-			--subdir="$(SUBDIR)" \
-			--source="$(FILE)" \
-			--hash="$(MIRROR_HASH)" \
-			--submodules $(SUBMODULES) \
-		|| ( $(call DownloadMethod/rawgit) ); \
-	)
+	$(SCRIPT_DIR)/dl_github_archive.py \
+		--dl-dir="$(DL_DIR)" \
+		--url="$(URL)" \
+		--version="$(SOURCE_VERSION)" \
+		--subdir="$(SUBDIR)" \
+		--source="$(FILE)" \
+		--hash="$(MIRROR_HASH)" \
+		--submodules $(SUBMODULES) \
+	|| ( $(call DownloadMethod/rawgit) )
 endef
 
 # Only intends to be called as a submethod from other DownloadMethod

qxwd1123/immortalwrt

include/download.mk
@@ -195,22 +195,18 @@ define DownloadMethod/svn
 endef
 
 define DownloadMethod/git
-	$(call wrap_mirror,$(1),$(2), \
-		$(call DownloadMethod/rawgit) \
-	)
+	$(call DownloadMethod/rawgit)
 endef
 
 define DownloadMethod/github_archive
-	$(call wrap_mirror,$(1),$(2), \
-		$(SCRIPT_DIR)/dl_github_archive.py \
-			--dl-dir="$(DL_DIR)" \
-			--url="$(URL)" \
-			--version="$(VERSION)" \
-			--subdir="$(SUBDIR)" \
-			--source="$(FILE)" \
-			--hash="$(MIRROR_HASH)" \
-		|| ( $(call DownloadMethod/rawgit) ); \
-	)
+	$(SCRIPT_DIR)/dl_github_archive.py \
+		--dl-dir="$(DL_DIR)" \
+		--url="$(URL)" \
+		--version="$(VERSION)" \
+		--subdir="$(SUBDIR)" \
+		--source="$(FILE)" \
+		--hash="$(MIRROR_HASH)" \
+	|| ( $(call DownloadMethod/rawgit) ); \
 endef
 
 # Only intends to be called as a submethod from other DownloadMethod